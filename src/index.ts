c3251fcbc2e43b69c7b93c3a23820acdd4d4a120
Mihail Kirov <shimonchick@gmail.com>
2022-02-22 19:57:06 +0200

    Polish API and add unit testing framework

--- src/index.ts
+++ src/index.ts
@@ -33,114 +33,153 @@ const db = getFirestore();
 
 const PORT = 4000;
 
-let app: Express.Application = Express();
-app.use(Express.urlencoded({ extended: false }));
+const fillProductData = (unparsedProductList: ShopifyProductData[], outProducts: Product[], uri: string) => {
+    unparsedProductList.forEach((productData: ShopifyProductData) => {
+        const product: Product = {
+            name: parse(productData.title).text.replace(/\n/g, " ") || "unknown",
+            price: productData.variants
+                && productData.variants.length > 0
+                && productData.variants[0].price
+                && parseFloat(productData.variants[0].price) || 0,
+            link: productData.handle && uri + "/products/" + productData.handle || "unknown",
+            brand: productData.vendor || "unknown",
+            description: productData.body_html && parse(productData.body_html).text.replace(/\n/g, " ") || "unknown",
+            domain: uri,
+            images: productData.images
+                && productData.images.length > 0
+                && productData.images.map((img: ShopifyProductImage) => img && img.src) || []
+        }
+
+        outProducts.push(product);
+    })
+}
+
+export const createApp = () => {
+    let app: Express.Application = Express();
+    app.use(Express.urlencoded({ extended: false }));
 //TODO: set up cors options
-const options: cors.CorsOptions = {
-    origin: "*",
-};
-app.use(cors(options));
-app.put("/domain", async (req: Express.Request, res: Express.Response) => {
-    if(!req.body || !req.body.domainUri)
-    {
-        //TODO: set proper response statuses
-        res.status(400).send("Please specify domain to be added to the scraper list");
-        return;
-    }
-
-    let uri = req.body.domainUri.trim().toLowerCase();
-    if(!uri || uri === "")
-    {
-        res.status(400).send("Invalid domain uri.");
-        return;
-    }
-    if(/^[a-z0-9]+\.[a-z]+$/.test(uri) == false)
-    {
-        res.status(400).send("Please send a domain in the format [name].[extention]");
-        return;
-    }
-    console.log(uri);
-
-    //TODO: abstract for different store types
-    //TODO: get store type from uri
-    //TODO: check valid url
-    try{
-        const response = await fetch("https://www." + uri + "/products.json");
-        if(!response)
+    const options: cors.CorsOptions = {
+        origin: "*",
+    };
+    app.use(cors(options));
+    app.options('*', cors(options));
+    app.put("/domain", async (req: Express.Request, res: Express.Response) => {
+        if(!req.body || !req.body.domainUri)
         {
-            res.status(500).send("Could not find information for given domain");
+            //TODO: set proper response statuses
+            res.status(400).send("Please specify domain to be added to the scraper list");
             return;
         }
 
-        const data = await response.json();
-        if(!data || !data.products || data.products.length == 0)
+        let uri = req.body.domainUri.trim().toLowerCase();
+        if(!uri || uri === "")
         {
-            res.status(500).send("Could not find any products for given domain")
+            res.status(400).send("Invalid domain uri.");
             return;
         }
-        let products: Product[] = [];
-        data.products.forEach((productData: ShopifyProductData) => {
-            const product: Product = {
-                name: parse(productData.title).text.replace(/\n/g, " ") || "unknown",
-                price: productData.variants
-                    && productData.variants.length > 0
-                    && productData.variants[0].price
-                    && parseFloat(productData.variants[0].price) || 0,
-                link: productData.handle && uri + "/products/" + productData.handle || "unknown",
-                brand: productData.vendor || "unknown",
-                description: productData.body_html && parse(productData.body_html).text.replace(/\n/g, " ") || "unknown",
-                domain: uri,
-                images: productData.images
-                    && productData.images.length > 0
-                    && productData.images.map((img: ShopifyProductImage) => img && img.src) || []
+
+        //TODO: abstract for different store types
+        //TODO: get store type from uri
+        //TODO: check valid url
+        try{
+            const uriObject = new URL(uri);
+            const initialUri = uriObject.protocol + "//" + uriObject.host + "/products.json";
+            console.log(initialUri);
+            const finalDomain = await fetch(initialUri, {method: 'GET'});
+            console.log(finalDomain);
+            if(!finalDomain)
+            {
+                res.status(400).send("Invalid domain uri. Please send full URI to fetch");
             }
-            products.push(product);
-        })
+            const response = await fetch(finalDomain.url, {method: 'GET'});
+            if(!response)
+            {
+                res.status(500).send("Could not find information for given domain");
+                return;
+            }
 
-        const docRef =  db.collection('products').doc(uri);
-        await docRef.set(Object.assign({}, products), {merge: true})
+            const data = await response.json();
+            //console.log(data);
+            if(!data)
+            {
+                res.status(500).send("Could not find any products for given domain")
+                return;
+            }
+            let products: Product[] = [];
+            if(data.products)
+            {
+                fillProductData(data.products, products, uri);
+            }
+            else if(data.length > 0)
+            {
+                fillProductData(data, products, uri);
+            }
+            else
+            {
+                res.status(500).send("Could not parse product data")
+                return;
+            }
 
-        res.status(200).send("OK");
+            const hostName = url.parse(uri).hostname;
+            if(!hostName)
+            {
+                res.status(400).send("Couldn't parse domain uri.");
+                return;
+            }
+            const docRef =  db.collection('products').doc(hostName);
+            await docRef.set(Object.assign({}, products), {merge: true})
 
-    } catch (e)
-    {
-        console.log(e);
-        res.status(500).send("Could not fetch info for domain");
-        return;
-    }
-})
+            res.status(200).send("OK");
 
-app.get("/products/:domainUri", async (req, res) => {
-    if(!req.params.domainUri)
-    {
-        res.status(400).send("please specify domain to get products for");
-        return;
-    }
+        } catch (e)
+        {
+            console.log(e);
+            res.status(500).send("Could not fetch info for domain");
+            return;
+        }
+    })
 
-    const uri = req.params.domainUri.trim().toLowerCase()
-    if(/([a-z0-9]+\.)*[a-z0-9]+\.[a-z]+/.test(uri) == false)
-    {
-        res.status(400).send("Please send a domain in the format www.something.com");
-        return;
-    }
-    if(!uri)
-    {
-        res.status(400).send("Invalid domain uri.");
-        return;
-    }
-    const docRef: FirebaseFirestore.DocumentReference = db.collection('products').doc(uri);
-    const docSnapshot: FirebaseFirestore.DocumentSnapshot = await docRef.get();
-    if(!docSnapshot.exists)
-    {
-        res.status(404).send("Products not found for domain or domain data doesn't exist yet.");
-        return;
-    }
-    const data: FirebaseFirestore.DocumentData = docSnapshot.data();
-    const products: Product[] = Object.values(data); // convert indexed object to array
-    console.log(products);
-    res.status(200).send(products);
-});
+    app.get("/products/:domainUri", async (req, res) => {
+        try {
+            if(!req.params.domainUri)
+            {
+                res.status(400).send("please specify domain to get products for");
+                return;
+            }
 
+            const uri = req.params.domainUri.trim().toLowerCase()
+            const uriObject = new URL(uri);
+            if(!uri || uri === "")
+            {
+                res.status(400).send("Invalid domain uri.");
+                return;
+            }
+            if(!uriObject.hostname)
+            {
+                res.status(400).send("Couldn't parse domain uri.");
+                return;
+            }
+            //console.log(hostName)
+            const docRef: FirebaseFirestore.DocumentReference = db.collection('products').doc(uriObject.hostname);
+            const docSnapshot: FirebaseFirestore.DocumentSnapshot = await docRef.get();
+            if(!docSnapshot.exists)
+            {
+                res.status(404).send("Products not found for domain or domain data doesn't exist yet.");
+                return;
+            }
+            const data: FirebaseFirestore.DocumentData = docSnapshot.data();
+            const products: Product[] = Object.values(data); // convert indexed object to array
+            //console.log(products);
+            res.status(200).send(products);
+        } catch (e)
+        {
+            res.status(500).send("Could not get info for this domain")
+        }
+    });
+    return app;
+}
+
+const app = createApp();
 app.listen(process.env.PORT || PORT, () => {
     console.log(`App running on port: ${PORT}`)
 })
